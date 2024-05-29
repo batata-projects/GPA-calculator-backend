f0185ac1e5c83837e7ff9718cf633e0bbb8e4ea7
Jad Shaker <jaadshaker@gmail.com>
2024-05-29 18:13:15 +0300

    Update configuration and database access code

--- src/db/models/users.py
+++ src/db/models/users.py
@@ -1,11 +1,12 @@
 from typing import Optional
 
+from gotrue.types import User as SupabaseUser  # type: ignore
 from pydantic import BaseModel
 
 from src.db.models.utils import EmailStr, UuidStr
 
 
-class Users(BaseModel):
+class User(BaseModel):
     id: Optional[UuidStr] = None
     username: str
     email: EmailStr
@@ -13,3 +14,15 @@ class Users(BaseModel):
     last_name: str
     credits: int
     grade: float
+
+    @classmethod
+    def validate_supabase_user(cls, user: SupabaseUser) -> "User":
+        return cls(
+            id=user.id,
+            username=user.user_metadata["username"],
+            email=user.email,
+            first_name=user.user_metadata["first_name"],
+            last_name=user.user_metadata["last_name"],
+            credits=user.user_metadata["credits"],
+            grade=user.user_metadata["grade"],
+        )
