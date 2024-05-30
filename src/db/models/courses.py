f4b417e3c7e5ab484a07a6f289ecd1b600137485
Jad Shaker <jaadshaker@gmail.com>
2024-05-30 18:51:33 +0300

    Fix typo in courses.py model

--- src/db/models/courses.py
+++ src/db/models/courses.py
@@ -7,7 +7,7 @@ from src.db.models.utils import UuidStr
 
 class Course(BaseModel):
     id: Optional[UuidStr] = None
-    available_courses_id: UuidStr
+    available_course_id: UuidStr
     user_id: UuidStr
     grade: Optional[float] = None
     passed: Optional[bool] = None
