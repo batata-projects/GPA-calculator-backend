53a2b12d91a0597b5167465aa71602af5764dbe8
Rayan Fakhreddine <rmf40@mail.aub.edu>
2024-05-30 12:30:51 +0300

    Update SupabaseTables enum value for all_courses to available_courses and fixed naming of files/methods/identifiers/classes

--- src/db/tables.py
+++ src/db/tables.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class SupabaseTables(str, Enum):
-    ALL_COURSES = "all_courses"
+    AVAILABLE_COURSES = "all_courses"
     COURSES = "courses"
     TERMS = "terms"
     USERS = "users"
