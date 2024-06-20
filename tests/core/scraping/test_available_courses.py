ee5fe3bc0d77116fa519792509bacc8bb60e0ea2
Jad Shaker <jaadshaker@gmail.com>
2024-06-20 18:06:31 +0300

    clean the tests

--- tests/core/scraping/test_available_courses.py
+++ tests/core/scraping/test_available_courses.py
@@ -1,32 +1,20 @@
-from src.core.scraping.available_courses import AvailableCoursesScraper
-
-
 class TestAvailableCourses:
     def test_available_courses(self) -> None:
-        ACS = AvailableCoursesScraper(None, None)
-        assert True
         ...
+        assert True
 
     def test_set_cookie(self) -> None:
-        ACS = AvailableCoursesScraper(None, None)
-        ACS._set_cookie
-        assert True
         ...
+        assert True
 
     def test__fetch_available_courses(self) -> None:
-        ACS = AvailableCoursesScraper(None, None)
-        ACS._fetch_available_courses
-        assert True
         ...
+        assert True
 
     def test_fetch_available_courses(self) -> None:
-        ACS = AvailableCoursesScraper(None, None)
-        ACS.fetch_available_courses
-        assert True
         ...
+        assert True
 
     def test_create_available_courses(self) -> None:
-        ACS = AvailableCoursesScraper(None, None)
-        ACS.create_available_courses
-        assert True
         ...
+        assert True
--- tests/core/scraping/test_terms.py
+++ tests/core/scraping/test_terms.py
@@ -1,15 +1,8 @@
-from src.core.scraping.terms import TermsScraper
-
-
 class TestTermsScraper:
     def test_fetch_terms(self) -> None:
-        TS = TermsScraper(None)
-        TS.fetch_terms
-        assert True
         ...
+        assert True
 
     def test_create_terms(self) -> None:
-        TS = TermsScraper(None)
-        TS.create_terms
-        assert True
         ...
+        assert True
