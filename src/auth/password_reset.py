f0896094eea44de1004a9ae5f41883bb02719d96
Rayan Fakhreddine <rmf40@mail.aub.edu>
2024-06-26 16:23:48 +0300

    chore: Update email credentials in password reset module

--- src/auth/password_reset.py
+++ src/auth/password_reset.py
@@ -1,34 +1,36 @@
 # implement reset password and forgot password using supabase API
 
-from supabase import create_client
-from supabase.client import Client
-import os
-from fastapi import APIRouter, HTTPException, Request
+
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-from fastapi_mail import FastMail, MessageSchema, ConnectionConfig 
+from fastapi_mail import ConnectionConfig, FastMail, MessageSchema
 from pydantic import EmailStr
-from typing import Optional
-from config import Config
+from supabase import create_client
+
+from src.config import Config
+
 router = APIRouter()
 
 # Supabase credentials
 SUPABASE_URL = Config.SUPABASE.URL
 SUPABASE_KEY = Config.SUPABASE.KEY
+assert SUPABASE_URL is not None
+assert SUPABASE_KEY is not None
 supabase_client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # Email credentials
-EMAIL = os.getenv("EMAIL")
-PASSWORD = os.getenv("PASSWORD")
 conf = ConnectionConfig(
-    MAIL_USERNAME=EMAIL,
-    MAIL_PASSWORD=PASSWORD,
-    MAIL_FROM=EMAIL,
-    MAIL_PORT=587,
-    MAIL_SERVER="smtp.gmail.com",
-    MAIL_TLS=True,
-    MAIL_SSL=False,
+    MAIL_USERNAME="username",
+    MAIL_PASSWORD="**********",
+    MAIL_FROM="test@email.com",
+    MAIL_PORT=465,
+    MAIL_SERVER="mail server",
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
 )
+
 
 @router.post("/forgot_password")
 async def forgot_password(email: EmailStr) -> JSONResponse:
@@ -45,9 +47,9 @@ async def forgot_password(email: EmailStr) -> JSONResponse:
         subject="Reset Password",
         recipients=[email],
         body=f"Click this link to reset your password: http://localhost:3000/reset_password?token={token['access_token']}",
+        subtype="html",
     )
     fm = FastMail(conf)
     await fm.send_message(message)
 
     return JSONResponse(content={"message": "Email sent"})
-
