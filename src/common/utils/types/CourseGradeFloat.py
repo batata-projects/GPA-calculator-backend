e97bd6f04ea2555cc18ed23e036cbbdbc6a712cc
Jad Shaker <jaadshaker@gmail.com>
2024-07-12 19:32:01 +0300

    56-implement-course-withdrawal (#57)
    * Add -1 grade representing course withdraw

--- src/common/utils/types/CourseGradeFloat.py
+++ src/common/utils/types/CourseGradeFloat.py
@@ -5,7 +5,7 @@ from pydantic.functional_validators import BeforeValidator
 
 
 def validate_course_grade_float(v: float) -> float:
-    if v not in [0.0, 1, 1.3, 1.7, 2, 2.3, 2.7, 3, 3.3, 3.7, 4, 4.3]:
+    if v not in [-1, 0.0, 1, 1.3, 1.7, 2, 2.3, 2.7, 3, 3.3, 3.7, 4, 4.3]:
         raise ValueError(f"{v} is an invalid grade")
     return v
 
