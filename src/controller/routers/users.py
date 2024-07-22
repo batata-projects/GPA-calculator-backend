d50fdf33c4a8fb6bf93da4a6bfa5e7f57ed4376b
Jad Shaker <jaadshaker@gmail.com>
2024-07-22 12:51:40 +0300

    fix rounding issue in the backend (#82)

--- src/controller/routers/users.py
+++ src/controller/routers/users.py
@@ -30,8 +30,44 @@ async def get_dashboard(
                 status_code=status.HTTP_404_NOT_FOUND,
                 message="User not found",
             )
+        user_data = user.model_dump(exclude={"grade"})
+        user_data["gpa"] = 0
+        if user.counted_credits:
+            user_data["gpa"] = round(user.grade / user.counted_credits, ndigits=10)
+        del user_data["counted_credits"]
 
         courses = course_dao.get_by_query(user_id=user_id)
+
+        terms: dict[Any, Any] = {}
+
+        for course in courses:
+            term = course.term
+            if term not in terms:
+                terms[term] = {
+                    "name": " ".join(map(str, Course.convert_to_term_name(term))),
+                    "gpa": 0.0,
+                    "grade": 0.0,
+                    "credits": 0,
+                    "counted_credits": 0,
+                    "courses": {},
+                }
+            if course.graded and course.grade is not None and course.grade != -1:
+                terms[term]["grade"] += course.grade * course.credits
+                terms[term]["counted_credits"] += course.credits
+            if course.grade:
+                terms[term]["credits"] += course.credits
+            terms[term]["courses"][course.id] = course.model_dump(
+                exclude={"id", "user_id"}
+            )
+
+        for term in terms:
+            if terms[term]["counted_credits"]:
+                terms[term]["gpa"] = round(
+                    terms[term]["grade"] / terms[term]["counted_credits"],
+                    ndigits=10,
+                )
+            del terms[term]["grade"]
+            del terms[term]["counted_credits"]
 
         return APIResponse(
             status_code=status.HTTP_200_OK,
