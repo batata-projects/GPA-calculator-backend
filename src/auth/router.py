acfe8ac9dcfe575aa4a0c53d64a6ef6d31bf2d52
Jad Shaker <jaadshaker@gmail.com>
2024-07-26 15:56:05 +0300

    Jad/87 utilize refresh token for the access token (#90)
    * chore: Add refresh_token route to auth router
    
    * Implement refresh token tests
    
    * clean code

--- src/auth/router.py
+++ src/auth/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, status
 
 from src.auth.auth import login, register
 from src.auth.password_reset import reset_password
+from src.auth.refresh_token import refresh_token
 from src.auth.schemas import LoginRequest, RegisterRequest, ResetPasswordRequest
 from src.common.responses import APIResponse
 from src.common.responses.API_response import APIResponse
@@ -60,4 +61,20 @@ async def reset_password_route(
         message="Password change successful",
         status_code=status.HTTP_200_OK,
         data=reset_password(request, user_dao).model_dump(),
+    )
+
+
+@auth_router.post(
+    "/refresh-token",
+    response_class=APIResponse,
+    summary="Refresh Token",
+    description="Refresh user token",
+)
+async def refresh_token_route(
+    user_dao: UserDAO = Depends(get_user_dao),
+) -> APIResponse:
+    return APIResponse(
+        message="Token refresh successful",
+        status_code=status.HTTP_200_OK,
+        data=refresh_token(user_dao).model_dump(),
     )
