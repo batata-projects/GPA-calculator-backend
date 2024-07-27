0ca62f2d59bdfe54c6a0b72a3892289e4c7d36b4
Jad Shaker <jaadshaker@gmail.com>
2024-07-27 16:26:19 +0300

    feat: Add email validation to registration process
    Add email validation to the registration process to ensure that the email provided is valid. This helps prevent users from registering with invalid or non-existent email addresses, improving the overall quality of user data.

--- src/common/utils/validators/NameValidator.py
+++ src/common/utils/validators/NameValidator.py
@@ -1,4 +1,9 @@
 def validate_name(v: str) -> str:
-    if v:
-        return v.title()
+    # check that name is only of characters, not empty and capital
+    if not v.isalpha():
+        raise ValueError("Name must only contain characters")
+    if not v.strip():
+        raise ValueError("Name cannot be empty")
+    if not v[0].isupper():
+        raise ValueError("Name must start with a capital letter")
     return v
